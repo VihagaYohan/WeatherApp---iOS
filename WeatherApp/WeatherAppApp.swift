5a17a09bbbba146c0c4ea079583a10a1c8637cd7
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-06-05 19:23:35 +0530

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Yohan on 2024-06-05.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
